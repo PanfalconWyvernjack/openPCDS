7cc93cf1465b6b390fbfcdfbe5a05768e794cfaf
Panflacon Wyvernjack <Panfalcon.Wyvernjack@protonmail.com>
2022-08-14 22:35:12 -0400

    adding general stuff from PrimeVue

--- open-pcds-vue/src/main.ts
+++ open-pcds-vue/src/main.ts
@@ -2,4 +2,17 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+ 
+import 'primevue/resources/themes/saga-blue/theme.css' //theme
+import 'primevue/resources/primevue.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.component('Dialog', Dialog);
+app.component('Button', Button);
+app.mount('#app')
