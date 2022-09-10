4a1d458aa3ee3fdb0fa114677000085f65514612
PanfalconWyvernjack <93452670+PanfalconWyvernjack@users.noreply.github.com>
2022-09-10 19:51:44 -0400

    Time Flies - Adding a flavour with tailwind CSS

---
+++ oPCDS-vue-tailwind/src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+// from https://flowbite.com/docs/getting-started/vue/
+// Could also possibly just add the Tailwind directives to the main.css file.
+// also from https://tailwindcss.com/docs/installation
+import './input.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ oPCDS-vue-tailwind/src/views/FormBuilder.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <main>
+    HELLO THIS IS A FORMBUILDER
+    </main>
+</template>
+    
