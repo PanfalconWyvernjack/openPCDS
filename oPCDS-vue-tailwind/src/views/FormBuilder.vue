7fbe92c436a3935f8fbc140422d72186557745fd
PanfalconWyvernjack <93452670+PanfalconWyvernjack@users.noreply.github.com>
2022-10-02 14:42:32 -0400

    adding a default table to the form view

--- oPCDS-vue-tailwind/src/views/FormBuilder.vue
+++ oPCDS-vue-tailwind/src/views/FormBuilder.vue
@@ -6,5 +6,18 @@
     <main>
     HELLO THIS IS A FORMBUILDER
     </main>
+
+    <table>
+        <thead>
+            <th>Col 1</th>
+            <th>Col 2</th>
+            <th>Col 3</th>
+        </thead>
+        <tbody>
+            <td>Some information</td>
+            <td><a href="">A Link</a></td>
+            <td><Button>A button</Button></td>
+        </tbody>
+    </table>
 </template>
     
